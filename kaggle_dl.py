5da97b30a63030dd86a045a3ef3dab41640cd654
Jeremy Petty <91158393+jp3tty@users.noreply.github.com>
2025-04-03 12:27:44 -0700

    Adding EDA notebook

---
+++ kaggle_dl.py
@@ -0,0 +1,6 @@
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("shashwatwork/dataco-smart-supply-chain-for-big-data-analysis")
+
+print("Path to dataset files:", path)
