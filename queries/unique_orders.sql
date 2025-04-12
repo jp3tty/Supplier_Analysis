6b1f9de668253de7c3125f01afb4093ced52dd0a
Jeremy Petty <91158393+jp3tty@users.noreply.github.com>
2025-04-11 21:20:21 -0700

    Update query file

--- queries/unique_orders.sql
+++ queries/unique_orders.sql
@@ -3,3 +3,19 @@ USE supply_chain;
 SELECT
 COUNT(DISTINCT `Order Id`) AS total_unique_orders
 FROM supply_chain_data;
+
+SELECT
+COUNT(`Order Id`) AS total_entries
+FROM supply_chain_data;
+
+SELECT
+	COUNT(*)- COUNT(DISTINCT `Order Id`) AS number_of_duplicates
+FROM supply_chain_data;
+
+SELECT
+	`Order Id`,
+    COUNT(*) AS occurrence_count
+FROM supply_chain_data
+GROUP BY `Order Id`
+HAVING COUNT(*) > 1
+ORDER BY occurrence_count DESC;
